1a94f1d7116c1a62601766b2ce2e42c20701c13d
B-Pasqual <brunopasqual0@gmail.com>
2022-12-02 03:08:05 -0300

    exercício checando se forma um triângulo

---
+++ Bruno/checando_triangles/script.js
@@ -0,0 +1,18 @@
+/* Crie uma aplicação que solicite ao usuário o tamanho de cada um dos lados de um triângulo e o informe se os três valores informados podem realmente formar um triângulo.
+
+Dica: a soma de quaisquer dois lados de um triângulo sempre é maior que o tamanho do lado restante. */
+
+alert('Digite 3 valores para saber se eles formam um triângulo');
+let lado1 = prompt('digite o primeiro valor');
+let lado2 = prompt('digite o segundo valor');
+let lado3 = prompt('digite o terceiro valor');
+
+alert(
+  lado1 + lado2 < lado3
+    ? 'não formam'
+    : lado1 + lado3 < lado2
+    ? 'não formam'
+    : lado3 + lado2 < lado1
+    ? 'não formam'
+    : 'formam um triângulo'
+);
--- Bruno/exercise_birthday/script.js
+++ Bruno/exercise_birthday/script.js
@@ -3,7 +3,7 @@ const hoje = new Date();
 const mesAtual = hoje.getMonth(); */
 
 // Continue
-//!Data do aniversário
+//!Data do aniversário ---------------------------------------
 let dia = prompt('Digite o dia do seu aniversário');
 let mes = prompt('Digite o mês do seu aniversário');
 let ano = hoje.getFullYear();
@@ -13,12 +13,12 @@ let dataAniversario = new Date(`${mes}/${dia}/${ano}`);
 let diference = hoje.getTime() - dataAniversario.getTime();
 let diasCalculados = Math.floor(diference / (1000 * 3600 * 24));
 
-//! método para ver se é positivo ou negativo
+//! método para ver se é positivo ou negativo --------------
 console.log(Math.sign(diference));
 //se maior 1
 // se menor -1
 
-//! if else para ver a saída do código
+//! if else para ver a saída do código ----------------------
 if (Math.sign(diference) == 1) {
   alert(`Já se passaram ${diasCalculados} dias do seu aniversário`);
 } else {
