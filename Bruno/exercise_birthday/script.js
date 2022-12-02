1c5f720075bcd0f4fb07e6ec0efdfdcea888be49
B-Pasqual <brunopasqual0@gmail.com>
2022-12-02 01:28:47 -0300

    exercicio aniversário ok

--- Bruno/exercise_birthday/script.js
+++ Bruno/exercise_birthday/script.js
@@ -1,5 +1,25 @@
 const hoje = new Date();
-const diaAtual = hoje.getUTCDate();
-const mesAtual = hoje.getMonth();
+/* const diaAtual = hoje.getUTCDate();
+const mesAtual = hoje.getMonth(); */
 
-// Continue daqui
+// Continue
+//!Data do aniversário
+let dataAniversario = new Date('06/30/2022');
+
+let diference = hoje.getTime() - dataAniversario.getTime();
+let proximoAniversario = new Date('06/30/2023');
+let diferenceNext = proximoAniversario.getTime() - hoje.getTime();
+
+//Calculando a quantidade de dias desde o meu último aniversário
+console.log(
+  `Já se passaram: ${Math.floor(
+    diference / (1000 * 3600 * 24)
+  )} dias desde o meu aniversário`
+);
+
+//Calculando a quantidade de dias até meu próximo aniversário
+console.log(
+  `Faltam ${Math.floor(
+    diferenceNext / (1000 * 3600 * 24)
+  )} dias até meu próximo aniversário`
+);
